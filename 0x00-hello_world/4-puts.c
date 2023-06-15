00d366ebb5c59f1c76d1172bf2d6845846def4df
brahimlk <lafrik.brahim@gmail.com>
2023-06-15 15:31:10 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/*puts function*/
-int main (void)
+/*
+ * puts function
+ *
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }

fb88a606328f51889f6044fbd802566c299f257c
brahimlk <lafrik.brahim@gmail.com>
2023-06-21 16:12:10 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabet in lowercase
+ *
+ * @i, j: input numbers
+ * Return: result
+ */
+int add(int i, int j)
+	{
+		int result;
+
+		result = i + j;
+		return(result);
+	}

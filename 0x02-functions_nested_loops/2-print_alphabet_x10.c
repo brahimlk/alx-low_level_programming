779d52a9f576894c4a89767f370679018b414d1e
brahimlk <lafrik.brahim@gmail.com>
2023-06-21 09:05:05 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,12 @@
  *
  * Return: 0
  */
-void print_alphabet(void);
 void print_alphabet_x10(void)
 	{
 		int j;
 
 		for (j = 0; j < 10; j++)
 		{
-			print_alphabet();
+			void print_alphabet(void);
 		}
 	}

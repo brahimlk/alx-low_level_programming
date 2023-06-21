c97c2b171a6899fbc94730fb64eda967fd88ceed
brahimlk <lafrik.brahim@gmail.com>
2023-06-21 16:55:56 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,25 +14,21 @@ void print_to_98(int n)
 		{
 			for (i = n; i <= 98; i++)
 			{
-				printf("%d", i);
 				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-
+					printf("%d, ", i);
+				else if (i == 98)
+					printf("%d", i);
 			}
 		}
 		else
 		{
 			for (i = n; i >= 98; i--)
 			{
-				printf("%d", i);
 				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+					printf("%d, ", i);
+				else if (i == 98)
+					printf("%d", i);
 			}
 		}
+		_putchar('\n');
 	}

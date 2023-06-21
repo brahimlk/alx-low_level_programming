059dcc9fdb12a74cd1f641618684d8416c2128c7
brahimlk <lafrik.brahim@gmail.com>
2023-06-21 07:45:23 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabet in lowercase 10x times
+ *
+ * Description: function that prints the alphabet 10x times, in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+	{
+		int i, j;
+
+		for (j = 0; j < 10; j++)
+		{
+			for (i = 97; i <= 122; i++)
+				_putchar(i);
+			_putchar('\n');
+		}
+	}

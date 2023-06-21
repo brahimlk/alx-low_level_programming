141ca716bed917e6ab15c002d9e258b38bac2cd4
brahimlk <lafrik.brahim@gmail.com>
2023-06-21 15:16:26 -0700

    8. every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ void jack_bauer(void)
 		{
 			for (j = 0; j < 60; j++)
 			{
-				_putchar(i / 10);
-				_putchar(i % 10);
+				_putchar((i / 10) + 48);
+				_putchar((i % 10) + 48);
 				_putchar(':');
-				_putchar(j / 10);
-				_putchar(j % 10);
+				_putchar((j / 10) + 48);
+				_putchar((j % 10) + 48);
 				_putchar('\n');
 			}
 		}

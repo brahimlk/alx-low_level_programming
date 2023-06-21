8e1ad96d94c827fe52f46014d7c7f3dbbc7d9ce9
brahimlk <lafrik.brahim@gmail.com>
2023-06-21 11:09:48 -0700

    7. last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,8 @@
 int print_last_digit(int n)
 {
 	if (n < 0)
-	{
 		n = -n;
-		n = n % 10;
-		_putchar(n + '0');
-		return (n);
-	}
-	else
-	{
-		n = n % 10;
-		return (n);
-	}
+	n = n % 10;
+	_putchar(n + '0');
+	return (n);
 }

97a70d01240453a64679b291409a4041d5fb6e69
brahimlk <lafrik.brahim@gmail.com>
2023-06-20 17:18:01 -0700

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,15 +4,14 @@
  *
  * Description: function that checks for lowercase character
  *
- * c: input character
+ * @c: input character
  * Return: 0 or 1
  */
 int _islower(int c)
 {
 	c = 'c';
 	if (c <= 122 && c >= 97)
-		_putchar('1');
-	if (!(c <= 122 && c >= 97))
-		_putchar('0');
-	return (0);
+		return(1);
+	else
+		return (0);
 }

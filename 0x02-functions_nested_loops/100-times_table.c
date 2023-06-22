cc81827ee4d8cd2d57ededd173534f4e913a0a1a
brahimlk <lafrik.brahim@gmail.com>
2023-06-21 17:46:12 -0700

    12. The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,44 +4,44 @@
  * @n: times table
  */
 void print_times_table(int n)
-	{
-		int j, i, r;
+{
+	int j, i, r;
 
-		if (n <= 15 && n >= 0)
+	if (n <= 15 && n >= 0)
+	{
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			r = 0;
+			for (i = 0; i <= n; i++)
 			{
-				r = 0;
-				for (i = 0; i <= n; i++)
+				if (r < 10)
+					_putchar(r + '0');
+				else if (r < 100)
 				{
-					if (r < 10)
-						_putchar(r + '0');
-					else if (r < 100)
-					{
-						_putchar((r / 10) + '0');
-						_putchar((r % 10) + '0');
-					}
-					else if (r >= 100)
-					{
-						_putchar((r / 100) + '0');
-						_putchar(((r / 10) % 10) + '0');
-						_putchar((r % 10) + '0');
-					}
-					r += j;
-					if (i != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-						if (r < 10)
-						{
-							_putchar(' ');
-							_putchar(' ');
-						}
-						else if (r < 100)
-							_putchar(' ');
-					}
+					_putchar((r / 10) + '0');
+					_putchar((r % 10) + '0');
 				}
-				_putchar('\n');
+				else if (r >= 100)
+				{
+					_putchar((r / 100) + '0');
+					_putchar(((r / 10) % 10) + '0');
+					_putchar((r % 10) + '0');
+				}
+				r += j;
+				if (i != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (r < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else if (r < 100)
+						_putchar(' ');
+				}
 			}
+			_putchar('\n');
 		}
 	}
+}

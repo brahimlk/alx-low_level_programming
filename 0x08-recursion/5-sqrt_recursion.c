4b973b43f9f53b7dc28246e11fa22363e0ed05a3
brahimlk <lafrik.brahim@gmail.com>
2023-07-09 07:08:29 -0700

    with recursion instead of loop

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _sqrt_recursion - function that returns the natural square root of a number.
  * @n: integer
- * Return 0 or -1
+ * Return: 0 or -1
  */
 int _sqrt_recursion(int n)
 {
@@ -10,10 +10,22 @@ int _sqrt_recursion(int n)
 
 	if (n < 0)
 		return (-1);
-	while (sqrt * sqrt < n)
-		sqrt++;
+	sqrt = _sqrt(n, sqrt)
 	if (sqrt * sqrt == n)
 		return (sqrt);
 	else
 		return (-1);
+}
+/**
+ * _sqrt - function that test sqrt
+ * @n: integer
+ * @sqrt: test sqrt
+ * Return: sqrt
+ */
+int _sqrt(int n, int sqrt)
+{
+	if (sqrt * sqrt >= n)
+		return (sqrt);
+	sqrt++;
+	return (_sqrt(n, sqrt));
 }

4655407d48bceaf215221c5226cf8699c86a64f2
brahimlk <lafrik.brahim@gmail.com>
2023-07-11 14:31:47 -0700

    fix for null string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,10 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, size;
 	char *ptr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	size = strlen(s1) + strlen(s2) + 1;
 	ptr = malloc(size * sizeof(char));
 	if (ptr == NULL)

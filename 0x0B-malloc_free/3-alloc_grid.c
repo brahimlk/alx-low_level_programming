4e4ce122f4db5a9b1dd45ddff4fb65187f1029f9
brahimlk <lafrik.brahim@gmail.com>
2023-07-11 09:52:33 -0700

    3. If you even dream of beating me youd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,22 +5,22 @@
  * of integers.
  * @width: width of array
  * @height: height of array
- * return: null or pointer
+ * Return: null or pointer
  */
 int **alloc_grid(int width, int height)
 {
 	int i, j;
 	int **ptr;
 
-	if (width <= 0 || height <=0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = malloc(width * height * sizeof(int));
+	ptr = malloc(sizeof(int[width][height]));
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < width; i++)
 	{
 		for (j = 0; j < height; j++)
-			ptr[i * height + j] = 0;
+			ptr[i][j] = 0;
 	}
 	return (ptr);
 }

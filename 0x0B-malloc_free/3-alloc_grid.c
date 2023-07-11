2d16e923dfbbc7f9866187b884481076d0ec172e
brahimlk <lafrik.brahim@gmail.com>
2023-07-11 10:19:06 -0700

    fix code: malloc 2d pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,12 +14,20 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = malloc(sizeof(int[width][height]));
+	ptr = malloc(height * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < height; j++)
+		ptr[i] = malloc(width * sizeof(int));
+		if (ptr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(ptr[i]);
+			free(ptr);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
 	}
 	return (ptr);

243c7390863246ab6c8b705efb48e3c8429d3ef4
brahimlk <brahim.lafrik@gmail.com>
2023-10-15 09:42:19 -0700

    betty style fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - information about dog's name, age and owner.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner.
+ */
 struct dog
 {
 	char *name;

576912e47522fd04b46892183b3808386f7c96ab
brahimlk <brahim.lafrik@gmail.com>
2023-10-15 12:49:20 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,14 +17,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc(strlen(name) * sizeof(char));
+	dog->name = malloc((strlen(name) + 1) * sizeof(char));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 	strcpy(dog->name, name);
-	dog->owner = malloc(strlen(owner) * sizeof(char));
+	dog->owner = malloc((strlen(owner) + 1) * sizeof(char));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);

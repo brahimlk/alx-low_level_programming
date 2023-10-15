26dbbeaaecc998c2ccecd8783e1f3fbb53b6d0a3
brahimlk <brahim.lafrik@gmail.com>
2023-10-15 09:57:11 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - information about dog's name, age and owner.
  * @name: dog's name.
@@ -12,5 +13,6 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif

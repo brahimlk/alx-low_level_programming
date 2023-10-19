75d43eaed74f59cb4cb3aa35b5c3f7fb72ca0fea
brahimlk <brahim.lafrik@gmail.com>
2023-10-19 04:45:07 -0700

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "calc.h"
+#include "stdio.h"
+#include "stdlib.h"
+/**
+ * main - program that performs simple operations.
+ *
+ * @argc: arg count.
+ * @argv: arg vector.
+ *
+ * Return: op result.
+ */
+int main(int argc, char **argv)
+{
+	int a = atoi(argv[1]);
+	int b = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((argv[2] == "/" || argv[2] == "%") && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if ((get_op_func(argv[2]))(a, b) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	return ((get_op_func(argv[2]))(a, b));
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "calc.h"
+/**
+ * op_add - sum of a and b.
+ *
+ * @a: integer.
+ * @b: integer.
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - difference of a and b.
+ *
+ * @a: integer.
+ * @b: integer.
+ *
+ * Return: difference.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - product of a and b.
+ * @a: integer.
+ * @b: integer.
+ *
+ * Return: product.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - result of the division of a by b.
+ * @a: integer.
+ * @b: integer.
+ *
+ * Return: result of div.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - remainder of the division of a by b.
+ *
+ * @a: integer.
+ * @b: integer.
+ *
+ * Return: remainder.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

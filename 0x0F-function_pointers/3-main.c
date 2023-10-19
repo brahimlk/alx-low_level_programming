f86966a1c20d006a9f26b008dd43410434c5dd03
brahimlk <brahim.lafrik@gmail.com>
2023-10-19 06:04:10 -0700

    fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(100);
 	}
-	if (get_op_func(argv[2])(a, b) == NULL)
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);

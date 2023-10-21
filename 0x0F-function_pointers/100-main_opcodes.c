91e88e170a8c5651cae7fad9a0db16953af6b49d
brahimlk <brahim.lafrik@gmail.com>
2023-10-21 06:24:17 -0700

    task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main -  program that prints the opcodes of its own main function.
+ * @argc: count.
+ * @argv: vector.
+ * Return: 0 or exit 1, 2.
+ */
+int main(int argc, char **argv)
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	opcodes_print((char *)&main, n);
+	return (0);
+}
+/**
+ * opcodes_print - funtion print opcodes of given function.
+ * @f: function address.
+ * @n: number of bytes
+ */
+void opcodes_print(char *f, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("\.2hhx", f[i]);
+		if (i != n - 1)
+			printf(" ");
+	}
+	printf("\n");
+}

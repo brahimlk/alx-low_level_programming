b87438cd4254fa77713e96cdc67aa2fc9fef1e0d
brahimlk <brahim.lafrik@gmail.com>
2023-10-21 10:23:26 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,10 +6,10 @@
 void print_all(const char * const format, ...)
 {
 	format_t conv_spec[] = {
-		{"c", "char"},
-		{"i", "int"},
-		{"f", "float"},
-		{"s", "char *"},
+		{'c', "char"},
+		{'i', "int"},
+		{'f', "float"},
+		{'s', "char *"},
 		{NULL, NULL}
 	};
 	int i = 0;
@@ -21,7 +21,7 @@ void print_all(const char * const format, ...)
 	{
 		while (conv_spec[j].flag != NULL)
 		{
-			if (strcmp(format[i], conv_spec[j].flag) == 0)
+			if (format[i] ==  conv_spec[j].flag)
 			{
 				printf(conv_spec[j].flag, va_arg(args, conv_spec[j].type));
 				if (format[i + 1] != '\0')
